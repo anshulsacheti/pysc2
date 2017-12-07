f99691185c7670143b683937ada035ef1bd95534
David Kuhta <davidkuhta@gmail.com>
2017-12-06 23:45:30 -0500

    daSC2 Scrapper

--- pysc2/bin/replay_actions.py
+++ pysc2/bin/replay_actions.py
@@ -25,7 +25,7 @@ import signal
 import sys
 import threading
 import time
-from datetime import datetime
+import json
 
 from future.builtins import range  # pylint: disable=redefined-builtin
 import six
@@ -37,89 +37,31 @@ from pysc2.lib import point
 from pysc2.lib import protocol
 from pysc2.lib import remote_controller
 
-from pysc2.lib import app
-import gflags as flags
+from absl import app
+from absl import flags
 from pysc2.lib import gfile
 from s2clientprotocol import common_pb2 as sc_common
 from s2clientprotocol import sc2api_pb2 as sc_pb
-
-import numpy as np
-import json
 
 FLAGS = flags.FLAGS
 flags.DEFINE_integer("parallel", 1, "How many instances to run in parallel.")
 flags.DEFINE_integer("step_mul", 8, "How many game steps per observation.")
 flags.DEFINE_string("replays", None, "Path to a directory of replays.")
-flags.DEFINE_integer("print_time", 100, "Interval between stat prints and data saves in seconds")
 flags.mark_flag_as_required("replays")
-FLAGS(sys.argv)
 
 
-screen_size = point.Point(84,84)
-minimap_size = point.Point(16, 16)
+size = point.Point(16, 16)
 interface = sc_pb.InterfaceOptions(
     raw=True, score=False,
     feature_layer=sc_pb.SpatialCameraSetup(width=24))
-screen_size.assign_to(interface.feature_layer.resolution)
-minimap_size.assign_to(interface.feature_layer.minimap_resolution)
+size.assign_to(interface.feature_layer.resolution)
+size.assign_to(interface.feature_layer.minimap_resolution)
 
 
 def sorted_dict_str(d):
   return "{%s}" % ", ".join("%s: %s" % (k, d[k])
                             for k in sorted(d, key=d.get, reverse=True))
 
-def calc_armies(screen):
-  friendly_army = []
-  enemy_army = []
-  unit_list = np.unique(screen[6])
-  for unit in unit_list:
-      friendly_pixels = (screen[5] == 1) & (screen[6] == unit)
-      friendly_unit_count = sum(screen[11,friendly_pixels])
-      #only append if count > 0
-      if friendly_unit_count:
-        friendly_army.append([int(unit),friendly_unit_count])
-      enemy_pixels = (screen[5] == 4) & (screen[6] == unit)
-      enemy_unit_count = sum(screen[11,enemy_pixels])
-      if enemy_unit_count:
-        enemy_army.append([int(unit), enemy_unit_count])
-  return friendly_army, enemy_army
-
-def update_minimap(minimap,screen):
-    #Update minimap data with screen details
-    #Identify which minimap squares are on screen
-    visible = minimap[1] == 1
-    #TODO: need to devide screen into visible minimap, for now
-    #devide each quantity by number of visible minimap squares
-    total_visible = sum(visible.ravel())
-    #power
-    minimap[4,visible] = (sum(screen[3].ravel())/
-                          (len(screen[3].ravel())*total_visible))
-    #friendy army
-    friendly_units = screen[5] == 1
-    #unit density
-    minimap[5,visible] = sum(screen[11,friendly_units])/total_visible
-    #Most common unit
-    if friendly_units.any() == True:
-        minimap[6,visible] = np.bincount(screen[6,friendly_units]).argmax()
-    else:
-        minimap[6,visible] = 0
-    #Total HP + Shields
-    minimap[7,visible] = ((sum(screen[8,friendly_units]) + 
-                          sum(screen[10,friendly_units]))/total_visible)
-    #enemy army
-    enemy_units = screen[5] == 4
-    #unit density
-    minimap[8,visible] = sum(screen[11,enemy_units])/total_visible
-    #main unit
-    if enemy_units.any() == True:
-        minimap[9,visible] = np.bincount(screen[6,enemy_units]).argmax()
-    else:
-        minimap[9,visible] = 0
-    #Total HP + shields
-    minimap[10,visible] = ((sum(screen[8,enemy_units]) + 
-                            sum(screen[10,friendly_units]))/total_visible)
-
-    return minimap
 
 class ReplayStats(object):
   """Summary stats of the replays seen so far."""
@@ -206,7 +148,7 @@ class ProcessStats(object):
                 time.time() - self.time))
 
 
-def valid_replay(info, ping, replay, replay_list):
+def valid_replay(info, ping):
   """Make sure the replay isn't corrupt, and is worth looking at."""
   if (info.HasField("error") or
       info.base_build != ping.base_build or  # different game version
@@ -214,10 +156,8 @@ def valid_replay(info, ping, replay, replay_list):
       len(info.player_info) != 2):
     # Probably corrupt, or just not interesting.
     return False
-  if replay[0:20] in replay_list:
-    return False
   for p in info.player_info:
-    if p.player_apm < 10 or p.player_mmr < 3440:
+    if p.player_apm < 10 or p.player_mmr < 1000:
       # Low APM = player just standing around.
       # Low MMR = corrupt replay or player who is weak.
       return False
@@ -233,14 +173,6 @@ class ReplayProcessor(multiprocessing.Process):
     self.run_config = run_config
     self.replay_queue = replay_queue
     self.stats_queue = stats_queue
-    self.replay_list = self.saved_replay_list()
-
-  def saved_replay_list(self):
-    replays = []
-    for root, dirs, files in os.walk("replay_state_data/"):
-            for name in files:
-                replays.append(name[0:20])
-    return replays
 
   def run(self):
     signal.signal(signal.SIGTERM, lambda a, b: sys.exit())  # Exit quietly.
@@ -271,16 +203,12 @@ class ReplayProcessor(multiprocessing.Process):
               self._print((" Replay Info %s " % replay_name).center(60, "-"))
               self._print(info)
               self._print("-" * 60)
-              if valid_replay(info, ping,replay_name,self.replay_list):
+              if valid_replay(info, ping):
                 self.stats.replay_stats.maps[info.map_name] += 1
                 for player_info in info.player_info:
-                  self.stats.replay_stats.races[
-                      sc_common.Race.Name(player_info.player_info.race_actual)] += 1
-                if info.player_info[0].player_result.result == 'Victory':
-                  winner = 1
-                else:
-                  winner = 2
-
+                  race_name = sc_common.Race.Name(
+                      player_info.player_info.race_actual)
+                  self.stats.replay_stats.races[race_name] += 1
                 map_data = None
                 if info.local_map_path:
                   self._update_stage("open map file")
@@ -288,25 +216,17 @@ class ReplayProcessor(multiprocessing.Process):
                 for player_id in [1, 2]:
                   self._print("Starting %s from player %s's perspective" % (
                       replay_name, player_id))
-                  race = sc_common.Race.Name(info.player_info[player_id-1].player_info.race_actual)
-                  if player_id == 1:
-                    enemy = 2
-                  else:
-                    enemy = 1 
-                  enemy_race = sc_common.Race.Name(info.player_info[enemy-1].player_info.race_actual)
                   self.process_replay(controller, replay_data, map_data,
-                                      player_id, replay_name, info.map_name,
-                                      winner,race,enemy_race)
+                                      player_id)
               else:
                 self._print("Replay is invalid.")
-                #self.stats.replay_stats.invalid_replays.add(replay_name)
+                self.stats.replay_stats.invalid_replays.add(replay_name)
             finally:
               self.replay_queue.task_done()
           self._update_stage("shutdown")
       except (protocol.ConnectionError, protocol.ProtocolError,
               remote_controller.RequestError):
-        pass
-        #self.stats.replay_stats.crashing_replays.add(replay_name)
+        self.stats.replay_stats.crashing_replays.add(replay_name)
       except KeyboardInterrupt:
         return
 
@@ -318,11 +238,8 @@ class ReplayProcessor(multiprocessing.Process):
     self.stats.update(stage)
     self.stats_queue.put(self.stats)
 
-  def process_replay(self, controller, replay_data, map_data,
-                     player_id, replay_id, map_name, winner,
-                     race, enemy_race):
+  def process_replay(self, controller, replay_data, map_data, player_id):
     """Process a single replay, updating the stats."""
-
     self._update_stage("start_replay")
     controller.start_replay(sc_pb.RequestStartReplay(
         replay_data=replay_data,
@@ -330,9 +247,10 @@ class ReplayProcessor(multiprocessing.Process):
         options=interface,
         observed_player_id=player_id))
 
-    #clear datafile
-    save_file = "replay_state_data/" + replay_id[0:20] + "_" + str(player_id) + '.json'
-    with open(save_file, 'w') as outfile: pass
+  # New file before write to append to later
+    jsonFilePath = "replayDir/" + "player" + player_id + "_" + self.stats.replay + ".json"
+    with open(jsonFilePath, 'w') as out:
+        pass
 
     feat = features.Features(controller.game_info())
 
@@ -343,7 +261,7 @@ class ReplayProcessor(multiprocessing.Process):
       self.stats.replay_stats.steps += 1
       self._update_stage("observe")
       obs = controller.observe()
-      actions = []
+
       for action in obs.actions:
         act_fl = action.action_feature_layer
         if act_fl.HasField("unit_command"):
@@ -359,16 +277,10 @@ class ReplayProcessor(multiprocessing.Process):
           self.stats.replay_stats.control_group += 1
 
         try:
-          full_act = feat.reverse_action(action)
-          func = full_act.function
-          args = full_act.arguments
+          func = feat.reverse_action(action).function
         except ValueError:
           func = -1
-          args = []
-
         self.stats.replay_stats.made_actions[func] += 1
-        actions.append([func,args])
-        
 
       for valid in obs.observation.abilities:
         self.stats.replay_stats.valid_abilities[valid.ability_id] += 1
@@ -379,26 +291,44 @@ class ReplayProcessor(multiprocessing.Process):
       for ability_id in feat.available_actions(obs.observation):
         self.stats.replay_stats.valid_actions[ability_id] += 1
 
-      all_features = feat.transform_obs(obs.observation)
-      #remove elevation, viz and selected data from minimap
-      minimap_data = all_features['minimap'][2:6,:,:]
-      screen = all_features['screen']
+    # New stuff
+      info = controller.replay_info(replay_data)
 
-      mini_shape = minimap_data.shape
-      minimap = np.zeros(shape=(11,mini_shape[1],mini_shape[2]),dtype=np.int)
-      minimap[0:4,:,:] = minimap_data
-      extended_minimap = update_minimap(minimap,screen).tolist()
-      friendly_army, enemy_army = calc_armies(screen)
-      full_state = [replay_id, map_name, player_id, extended_minimap,
-                    friendly_army,enemy_army,all_features['player'].tolist(),
-                    all_features['available_actions'].tolist(),actions,winner,
-                    race,enemy_race]
+    # Get player id/winner
+      wonGame = -1
+      if player_id == 1:
+          player = 0
+          enemy = 1
+          wonGame = 1 if info.player_info[player].player_result.result == 'Victory' else 2
+      else:
+          player = 1
+          enemy = 0
+          wonGame = 2 if info.player_info[player].player_result.result == 'Victory' else 1
 
-      
-      #save state to disk, line delimited
-      with open(save_file, 'a') as outfile:
-        json.dump(full_state, outfile)
-        outfile.write('\n')
+    # Get races
+      player_race = sc_common.Race.Name(info.player_info[player].player_info.race_actual)
+      enemy_race  = sc_common.Race.Name(info.player_info[enemy].player_info.race_actual)
+
+    # Replay name, map name
+      replay_name = self.stats.replay
+      map_name = info.map_name
+
+    # Feature layers
+      layers = feat.transform_obs(obs.observation)
+
+    # dump out all previous data plus feature layers in json
+      replay = layers # already has a ton of info
+      replay['player_race'] = player_race
+      replay['enemy_race'] = enemy_race
+      replay['replay_name'] = replay_name
+      replay['map_name'] = map_name
+      replay['wonGame'] = wonGame
+      replay['player'] = player
+      replay['enemy'] = enemy
+
+      with open(jsonFilePath, 'a') as out:
+        json.dump(replay, jsonFilePath)
+        out.write('\n')
 
       if obs.player_result:
         break
@@ -415,7 +345,7 @@ def stats_printer(stats_queue):
 
   running = True
   while running:
-    print_time += FLAGS.print_time
+    print_time += 10
 
     while time.time() < print_time:
       try:
